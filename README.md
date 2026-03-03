9117bf0df7c935978b552379857f951dfb5c63d9
Zackry12 <goodmean81@gmail.com>
2026-03-03 16:43:48 +0800

    Create a 16x16 grid

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+This project is a browser version of something between a sketchpad and an Etch-A-Sketch. It is created to test my DOM skill.
---
+++ main.js
@@ -0,0 +1,23 @@
+const container = document.createElement("div");
+container.classList.add('container')
+document.body.append(container)
+
+const grid = document.createElement('div');
+grid.classList.add('grid');
+
+for (let i=1;i<17;i++){
+    const gridRow = document.createElement('div');
+    for (let i=1;i<17;i++){
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridRow.appendChild(gridItem);
+    }
+    gridRow.classList.add('grid-row');
+    grid.appendChild(gridRow)
+}
+
+for (let i=1;i<17;i++){
+    
+}
+
+container.appendChild(grid);
