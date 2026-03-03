5965f18f0f7dafc85bcff836232a24ed35f0fc20
Zackry12 <goodmean81@gmail.com>
2026-03-03 17:30:32 +0800

    Add a grid size button
    The button take a sigle number input from the user and create the area

--- main.js
+++ main.js
@@ -4,19 +4,40 @@ document.body.append(container)
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
+const buttonList = document.createElement('div')
+buttonList.classList.add('button-list')
+const gridSizeButton = document.createElement('button')
+gridSizeButton.textContent = "Grid size"
+gridSizeButton.classList.add('grid-size-button')
+buttonList.appendChild(gridSizeButton)
 
-for (let i=1;i<17;i++){
-    const gridRow = document.createElement('div');
-    for (let i=1;i<17;i++){
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.addEventListener('mouseover',() => gridItem.style.backgroundColor = 'red')
-        gridRow.appendChild(gridItem);
+function gridMaker(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        const gridRow = document.createElement('div');
+        for (let i = 0; i < gridSize; i++){
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.addEventListener('mouseover',() => gridItem.style.backgroundColor = 'red')
+            gridRow.appendChild(gridItem);
+        }
+        gridRow.classList.add('grid-row');
+        grid.appendChild(gridRow)
     }
-    gridRow.classList.add('grid-row');
-    grid.appendChild(gridRow)
 }
+
+//create a default 16x16 grid
+gridMaker(16)
+
+//button functionality
+gridSizeButton.addEventListener('click',() => {
+    let gridSize = prompt("Enter a number between 1-100");
+    while (grid.firstChild){
+        grid.removeChild(grid.firstChild)
+    };
+    gridMaker(gridSize);
+});
 
 
 
 container.appendChild(grid);
+container.appendChild(buttonList);
